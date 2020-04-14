1ca952ec74dc1d50f8d05bedce8343623a68b2b0
Mohamad Dimas Radityatama <dimas@vioren.com>
2020-04-14 23:27:40 +0700

    seed ini dulu aja

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+        $this->call(UsersTableSeed::class);
+        $this->call(InformationsTableSeeder::class);
     }
 }
